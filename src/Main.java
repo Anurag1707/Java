d0a0a388a9f990650c342e44829c7f2a5ea0f566
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-11-03 11:28:48 +0530

    1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args){
+//        System.out.println("Hello Java");
+//        byte b=2;
+//        System.out.println(b);
+//         float a=1.2f;
+//        System.out.println(a);
+//        char c='A';
+//        System.out.println(c);
+//        boolean d=true;
+//        System.out.println(d);
+
+
+       int a=5;
+       for(int i=0;i<4;i++){
+           for(int j=0;j<=i;j++){
+               System.out.print(a);
+           }
+           System.out.println();
+       }
+    }
+}
