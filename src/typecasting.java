034e71c3e3d12ef0f8b8a75a354766c179784860
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-11-04 10:32:23 +0530

    type

---
+++ src/typecasting.java
@@ -0,0 +1,11 @@
+public class typecasting {
+    public static void main(String[] args){
+//        byte a=5;
+//        short b=a;
+//        System.out.println(b);
+
+        short c= 130;
+        byte d=(byte)c;
+        System.out.println(d);
+    }
+}
