f70f77cdb115be44fa44ade96642090d1d2e149a
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-11-04 10:09:10 +0530

    scan use

--- src/lect.java
+++ src/lect.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class lect {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter integer");
+        int a= sc.nextInt();
+
+        System.out.print("Enter Double");
+        double b= sc.nextDouble();
+
+        System.out.print("Enter single word");
+        String c= sc.next();
+
+        sc.nextLine();
+
+        System.out.print("Enter Full line: ");
+        String d= sc.nextLine();
+
+        System.out.println("Integer: " +a);
+        System.out.println("Double :" +b);
+        System.out.println("Single word:" +c);
+        System.out.println("Full line:" +d);
+    }
 }
