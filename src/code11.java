47f8169670f34ae22060e9bf460980a567846214
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-11-11 15:15:35 +0530

    code

---
+++ src/code11.java
@@ -0,0 +1,11 @@
+public class code11 {
+    int a=5; //not static variable
+    static int b=6;//static variable
+    public static void main(String[] args){
+          code11 obj=new code11();
+
+          System.out.println(obj.a);
+          System.out.println(b);
+
+    }
+}
---
+++ src/code12.java
@@ -0,0 +1,9 @@
+public class code12 {
+    void Sum(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args){
+        code12 obj=new code12();
+        obj.Sum();
+    }
+}
