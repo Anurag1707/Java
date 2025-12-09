3f81bff2223af8dfc48b10cba13a6f5c37c71381
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-12-09 14:11:32 +0530

    code 27

---
+++ src/code27.java
@@ -0,0 +1,16 @@
+public class code27 {
+    public static void main(String[] args){
+          System.out.println("hello");
+          try{
+              int a=5;
+              int b=0;
+              int c=a/b;
+              System.out.println(c);
+          }catch (Exception e) {
+              System.out.println(e);
+          }
+          System.out.println("byeeeee");
+
+
+    }
+}
