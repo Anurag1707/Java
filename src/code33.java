d90dda7afb69382fa06c44ccdee6a5419362a5a1
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-12-12 15:42:32 +0530

    code 33

---
+++ src/code33.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class code33 {
+    public static void main(String[] args){
+        ArrayList<Integer> list =new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(9);
+        list.add(8);
+        List<Integer> res= list.stream().filter(n-> n>6).toList();
+        List<Integer> mapData= list.stream().map(n-> n*2).toList();
+        int data= list.stream().reduce(0,(a,b)->a+b);
+        List<Integer> skip= list.stream().skip(2).toList();
+        List<Integer> limit= list.stream().limit(2).toList();
+        List<Integer> sort= list.stream().sorted((a,b)->a-b).toList();
+
+
+
+        System.out.println(res);
+        System.out.println(mapData);
+        System.out.println(data);
+        System.out.println(skip);
+        System.out.println(limit);
+        System.out.println(sort);
+
+
+
+
+
+    }
+}
